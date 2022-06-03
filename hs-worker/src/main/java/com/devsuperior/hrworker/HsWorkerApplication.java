17ce0944bf6eac5c63210c0e75f05f779818e906
Evan <evanildosilva467@gmail.com>
2022-06-03 11:19:52 -0300

    Project hr-worker created

---
+++ hs-worker/src/main/java/com/devsuperior/hrworker/HsWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsWorkerApplication.class, args);
+	}
+
+}
